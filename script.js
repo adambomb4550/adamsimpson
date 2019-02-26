7e35d9b45e16bd1b615569e5b5bea73abf30bc6f
adambomb4550 <adam.simpson@students.jmcss.org>
2019-02-26 10:30:24 -0600

    responsive now 2/26/18

--- script.js
+++ script.js
@@ -2,12 +2,65 @@ window.onload = function(){
   var d = new Date ();
   var month_name = ['January','February','March','April','May','June','July','August','September','October','November','December'];
   var month = d.getMonth();     //0-11
-  var year = d.getFullYear();    //2018
+  var year = d.getFullYear();    //2019
   var first_date = month_name[month] + " " + 1 + " " + year;
-  //February 1 2018
+  //February 1 2019
   var tmp = new Date(first_date). toDateString();
-  //Fri Feb 01 2018 ...
+  //Fri Feb 01 2019 ...
   var first_day = tmp.substring(0, 3);    //Fri
   var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
   var day_no = day_name.index0f(first_day);  //1
+  var days = new Date(year, month+1, 0).getDate();  //30
+  //Thu Feb 28 2019 ...
+  var calendar = get_calendar(day_no, days);
+  document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
+  document.getElementById("calendar-dates").appendChild(calendar);
+}
+
+function get_calendar(day_no, days) {
+  var table = document.createElement('table');
+  var tr = document.createElement('tr');
+
+  //row for the day letters
+  for(var c=0; c<=6; c++){
+    var td = document.createElement('td');
+    td.innerHTML = "SMTWTFS"[c];
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+
+  //create 2nd row
+tr = document.createElement('tr');
+for(c=0; c<=6; c++){
+  if(c == day_no){
+    break;
+  }
+  var td = document.createElement('td');
+  td.innerHTML = ""
+  tr.appendChild(td);
+}
+var count = 1;
+for(; c<=6; c++){
+  var td = document.createElement('td');
+  td.innerHTML = count;
+  count++;
+  tr.appendChild(td);
+}
+table.appendChild(tr);
+
+//rest of the date rows
+for(var r=3; 2<=6; r++){
+  tr = document.createElement('tr');
+  for(var c=0; c<=6; c++){
+    if(countt > days){
+      table.appendChild(tr);
+      return table;
+    }
+    var td = document.createElement('td');
+    td.innerHTML = count;
+    count++;
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
 }
